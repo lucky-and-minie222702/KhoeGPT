9d92188032352c8be18e5e50d810efd09630bb2f
techn0z <minhtrietamoled@gmail.com>
2025-10-05 06:22:13 +0700

    update

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ training_args = TrainingArguments(
     learning_rate = 5e-5,
     
     per_device_train_batch_size = 4,
-    per_device_eval_batch_size = 8,
+    per_device_eval_batch_size = 16,
 
     gradient_accumulation_steps = 8,
     eval_accumulation_steps = None,
