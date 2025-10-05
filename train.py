0fb9038e29e19644fff3c521f7092181d7ac14f6
techn0z <minhtrietamoled@gmail.com>
2025-10-05 09:29:25 +0700

    update

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ class MyDataset(Dataset):
         return inp
     
 df = pd.read_csv("train.csv")
-train_df, eval_df = train_test_split(df, test_size = 0.1, random_state = 22022009)
+train_df, eval_df = train_test_split(df, test_size = 0.0001, random_state = 22022009)
 
 train_ds = MyDataset(train_df, 1024)
 eval_ds = MyDataset(eval_df, 1024)
